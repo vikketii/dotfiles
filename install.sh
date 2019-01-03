cdfa2ab0860bdf56130bc3b1c9f422638e30e1ae
Vikke Tiirola <vikke.tiirola@gmail.com>
2019-01-03 16:04:43 +0200

    Backup script for old configs.

---
+++ install.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+CMD="$1"
+dotfilesdir=$(pwd)
+backupdir=~/.dotfiles.orig
+dotfiles=(.bash_aliases .bashrc .vimrc .Xresources)
+dotfiles_config=(.compton .i3 .i3status .rofi .zathura)
+
+
+printusage() {
+    prog=$(basename "$0")
+    echo "Usage: $prog [-option]" >&2
+    echo "" >&2
+    echo "Options:" >&2
+    echo "  --help        Print this message" >&2
+    echo "  -p            Install all packages" >&2
+    echo "  -c            Install all config" >&2
+}
+
+
+install_packages() {
+    pacman -S --needed - < "$dotfilesdir/packages/pkglist"
+}
+
+
+install_configs() {
+    # Backup configs
+    if ! [ -f $backupdir/check-backup.txt ]; then
+        mkdir -p $backupdir/.config
+        cd $backupdir
+        touch check-backup.txt
+
+        # Backup to ~/.dotfiles.orig
+        for dots in "${dotfiles[@]}"
+        do
+            /bin/cp -rf ~/${dots} $backupdir
+            # &> /dev/null
+        done
+
+        # Backup dotfiles_config to ~/.dotfiles.orig/.config
+        for dots_conf in "${dotfiles_config[@]//./}"
+        do
+            /bin/cp -rf ~/.config/${dots_conf} $backupdir/.config
+            # &> /dev/null
+        done
+
+        # Output
+        echo -e "Your config is backed up in "$backupdir"\n" >&2
+        echo -e "Please do not delete check-backup.txt in .dotfiles.orig folder." >&2
+        echo -e "It's used to backup and restore your old config.\n" >&2
+    fi
+
+    #mkdir -p ~/testing/.config
+    #for dots_conf in "${dotfiles_config[@]}"
+    #do
+    #    /bin/rm -rf ~/testing/.config/${dots_conf[@]//./}
+    #    /bin/ln -fs "$dotfilesdir/test/${dots_conf}" ~/testing/${dots_conf[@]//./}
+    #done
+
+    echo -e "\nYour dotfiles are not synced yet, but they are backed up."
+}
+
+
+if [[ -z "$1" ]] || [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
+    printusage
+    exit 0
+fi
+
+
+case "$CMD" in
+    -c)
+        read -p "Install configs? y/n " -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            install_configs
+        fi
+        ;;
+    -p)
+        read -p "Install packages? y/n " -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            install_packages
+        fi
+        ;;
+    *)
+        echo "Command not found" >&2
+        exit 1
+esac
